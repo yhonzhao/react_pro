f5eeb2a7c8a7e0dd3aeaba5067e412196208414f
yhon <yhon.zhao@qq.me>
2018-08-14 20:39:32 +0800

    修改403权限页

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 
 
 @connect(state=>({
-    toute: state.routing
+    route: state.routing
 }),{
     push
 })
--- app/containers/exception/403.js
+++ app/containers/exception/403.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import Exception from '../../component/Exception';
+import { goBack } from 'react-router-redux'
+import {connect} from 'react-redux';
+import { Button } from 'antd';
 
-export default () => (
-  <Exception type="403" />
-);
+@connect(state=>({
+  route: state.routing
+}),{
+  goBack
+})
+export default class PermissionDenied extends React.Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    const actions = (
+      <div>
+        <Button onClick={()=>this.props.goBack()} type="primary">返回上页</Button>
+      </div>
+    );
+    return(<Exception type="403" actions={actions} />)
+  }
+}
+
